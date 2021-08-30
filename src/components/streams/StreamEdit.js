2fa19bebd2319789a491e94be980248b766aae8d
Melissa Rice <melissa.rice@perficient.com>
2021-08-30 13:19:53 -0400

    changed streamedit to class component to fetchStream with matching id

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,13 +1,20 @@
 import React from "react";
 import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
-const StreamEdit = (props) => {
-  console.log(props);
-  return <div>StreamEdit</div>;
-};
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    console.log(this.props);
+    return <div>StreamEdit</div>;
+  }
+}
 
 const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps)(StreamEdit);
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
